b254185bfdbcbd766849d41dc188ec383a65a4c0
Maciej Płachta <maciej.plachta1@gmail.com>
2024-05-09 22:54:38 +0200

    created menu and game history

---
+++ BlackjackConsole/GameModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackConsole
+{
+    class GameModel
+    {
+        public int GameId { get; set; }
+        public string Winner {  get; set; }
+        public int PlayerPoints { get; set; }
+        public int ComputerPoints { get; set; }
+        public List<Card> PlayerCards = new List<Card>();
+        public List<Card> ComputerCards = new List<Card>();
+    }
+    class GameList
+    {
+        public List<GameModel> Games = new List<GameModel>();
+    }
+}
--- BlackjackConsole/Program.cs
+++ BlackjackConsole/Program.cs
@@ -4,110 +4,175 @@
     {
         static void Main(string[] args)
         {
+            bool isMenu = true;
             bool isGame = true;
             bool gameOver = false;
             Dealer dealer = new Dealer();
             Player player = new Player();
             Player computer = new Player();
+            GameList gameList = new GameList();
             int playerPoints = 0;
             int computerPoints = 0;
             int computerTotal = 0;
+            int gameNumber = 1;
             Console.WriteLine("Blackjack game! :)");
-            Console.Write("Press any key to start the game");
+            Console.Write("Press any key to enter the menu");
             Console.ReadLine();
-            while (isGame)
+            while (isMenu)
             {
-                Deck deck = new Deck();
-                while (gameOver == false)
+                Console.WriteLine("\n1-Start new game\n2-View game history\n3-Exit the game");
+                string playerDecision = Console.ReadLine();
+                switch(playerDecision)
                 {
-                    
-                    Console.Clear();
-                    deck.CardDeck = deck.BuildCardDeck();
-                    dealer.DealCard(deck, player, "Player");
-                    Player.ComputerDraw(computerTotal, deck, computer, dealer);
-                    playerPoints = player.GetPlayerPoints();
-                    computerPoints = computer.GetComputerPoints();
-                    computerTotal = computer.GetPlayerPoints();
-                    Console.WriteLine($"\n\n\nYour current points are {playerPoints} (with aces counting as 1)");
-                    Console.WriteLine($"Computer's current points are {computerPoints} (without the first card and aces counting as 1)");
-                    if (playerPoints >= 21)
-                    {
+                    case "1":
+                        Console.Clear();
+                        Game();
+                        Console.Clear();
                         break;
-                    }
-                    string playerDecision = "";
-                    while (playerDecision != "y" && playerDecision != "n")
+                    case "2":
+                        Console.Clear();
+                        History();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            void Game()
+            {
+                while (isGame)
+                {
+                    Deck deck = new Deck();
+                    GameModel game = new GameModel();
+                    while (gameOver == false)
                     {
-                        Console.Write("\n\nDo you want to draw? y/n: ");
-                        playerDecision = Console.ReadLine();
-                    }
-                    if (playerDecision == "n")
-                    {
-                        while (computerTotal < 16)
+
+                        Console.Clear();
+                        Console.WriteLine($"Game ID: {gameNumber}");
+                        deck.CardDeck = deck.BuildCardDeck();
+                        dealer.DealCard(deck, player, "Player");
+                        Player.ComputerDraw(computerTotal, deck, computer, dealer);
+                        playerPoints = player.GetPlayerPoints();
+                        computerPoints = computer.GetComputerPoints();
+                        computerTotal = computer.GetPlayerPoints();
+                        Console.WriteLine($"\n\n\nYour current points are {playerPoints} (with aces counting as 1)");
+                        Console.WriteLine($"Computer's current points are {computerPoints} (without the first card and aces counting as 1)");
+                        if (playerPoints >= 21)
                         {
-                            Player.ComputerDraw(computerTotal, deck, computer, dealer);
-                            computerTotal = computer.GetPlayerPoints();
+                            break;
                         }
-                        Console.WriteLine("\nComputer decides to hold");
-                        gameOver = true;
+                        string playerDecision = "";
+                        while (playerDecision != "y" && playerDecision != "n")
+                        {
+                            Console.Write("\n\nDo you want to draw? y/n: ");
+                            playerDecision = Console.ReadLine();
+                        }
+                        if (playerDecision == "n")
+                        {
+                            while (computerTotal < 16)
+                            {
+                                Player.ComputerDraw(computerTotal, deck, computer, dealer);
+                                computerTotal = computer.GetPlayerPoints();
+                            }
+                            Console.WriteLine("\nComputer decides to hold");
+                            gameOver = true;
+                        }
                     }
+                    Console.WriteLine("\n\n\nGame is over. Let's count the points");
+                    player.PlayerAces();
+                    computer.ComputerAces();
+                    playerPoints = player.GetPlayerPoints();
+                    computerTotal = computer.GetPlayerPoints();
+                    Console.WriteLine($"\nComputer's first card was {computer.playerHand.First().Rank} of {computer.playerHand.First().Suit}");
+                    game.Winner = WhoWins(playerPoints, computerTotal);
+                    game.PlayerPoints = playerPoints;
+                    game.ComputerPoints = computerTotal;
+                    game.PlayerCards = player.playerHand;
+                    game.ComputerCards = computer.playerHand;
+                    game.GameId = gameNumber;
+                    gameList.Games.Add(game);
+                    string decision = "";
+                    while (decision != "y" && decision != "n")
+                    {
+                        Console.Write("\n\n\nDo you want to play another game? y/n: ");
+                        decision = Console.ReadLine();
+                    }
+                    if (decision == "n")
+                    {
+                        isGame = false;
+                    }
+                    player.playerHand.Clear();
+                    computer.playerHand.Clear();
+                    computerTotal = 0;
+                    playerPoints = 0;
+                    computerPoints = 0;
+                    gameNumber++;
+                    gameOver = false;
                 }
-                Console.WriteLine("\n\n\nGame is over. Let's count the points");
-                player.PlayerAces();
-                computer.ComputerAces();
-                playerPoints = player.GetPlayerPoints();
-                computerTotal = computer.GetPlayerPoints();
-                Console.WriteLine($"\nComputer's first card was {computer.playerHand.First().Rank} of {computer.playerHand.First().Suit}");
-                WhoWins(playerPoints, computerTotal);
-                string decision = "";
-                while (decision != "y" && decision != "n")
-                {
-                    Console.Write("\n\n\nDo you want to play another game? y/n: ");
-                    decision = Console.ReadLine();
-                }
-                if (decision == "n")
-                {
-                    isGame = false;
-                }
-                player.playerHand.Clear();
-                computer.playerHand.Clear();
-                computerTotal = 0;
-                playerPoints = 0;
-                computerPoints = 0;
-                gameOver = false;
             }
             
+            void History()
+            {
+                if(gameList.Games.Count > 0)
+                {
+                    foreach(GameModel game in gameList.Games)
+                    {
+                        Console.WriteLine($"Game ID: {game.GameId}\nWinner: {game.Winner}\nPlayer points: {game.PlayerPoints}\nComputer points: {game.ComputerPoints}");
+                        Console.WriteLine($"Player cards: ");
+                        foreach(Card card in game.PlayerCards)
+                        {
+                            Console.Write($"{card.Rank} of {card.Suit}, "); //TODO
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No played games yet");
+                }
+                Console.Write("Press any key to return to the main menu");
+                Console.ReadLine();
+            }
         }
-        public static void WhoWins(int playerPoints, int computerTotal)
+        public static string WhoWins(int playerPoints, int computerTotal)
         {
             Console.WriteLine($"\n\n\nComputer's total points are {computerTotal}");
             Console.WriteLine($"\nPlayer's total points are {playerPoints}");
             if (playerPoints == 21)
             {
                 Console.WriteLine("\nYou win by having blackjack! Congrats!");
+                return "Player";
             }
             else if (playerPoints > 21)
             {
                 Console.WriteLine("\nYour total points are greater than 21! You lose!");
+                return "Computer";
             }
             else
             {
                 if (computerTotal > 21)
                 {
                     Console.WriteLine("You win, computer has more than 21");
+                    return "Player";
                 }
                 else
                 {
                     if (playerPoints > computerTotal)
                     {
                         Console.WriteLine("\nYou win by having more points than compuer!");
+                        return "Player";
                     }
                     else if (computerTotal > playerPoints)
                     {
                         Console.WriteLine("\nComputer wins by having more points than you!");
+                        return "Computer";
                     }
                     else
                     {
                         Console.WriteLine("\nDraw!");
+                        return "Draw";
                     }
                 }
 
